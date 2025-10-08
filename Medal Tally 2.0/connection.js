a879b89c0028d4c5e62d1a21a6177a15c458cb2c
cjsabaw <130220353+cjsabaw@users.noreply.github.com>
2025-10-08 13:42:38 +0800

    Create connection to supabase
    walakokabalo kinsa mo himo sa supabase. pag naa na nahimo i change lang mga values ari    - Sabaw

---
+++ Medal Tally 2.0/connection.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "Supabase link walakokablo kinsa mo himo";
+const SUPABASE_KEY = "YOUR-ANON-KEY";  // sabaw was here
+
+//making a connection
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
